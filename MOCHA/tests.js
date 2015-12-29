266b3b7f6d727aa9f11630747eef3c455415bae6
charlesfranca <charles-franca@live.com>
2015-12-29 12:09:31 -0200

    adding sinon to jasmine

---
+++ MOCHA/tests.js
@@ -0,0 +1,15 @@
+suite('shold be my first test', function(){
+
+	setup(function(){
+
+	});
+
+	teardown(function(){
+		
+	});
+
+	test('teste 1', function(){
+		expect(1).to.equal(1);
+	})
+
+});
--- jasmine/spec/site.spec.js
+++ jasmine/spec/site.spec.js
@@ -1,13 +1,30 @@
-describe("first test with jasmine-jquery", function(){
-	beforeEach(function(){
-		jasmine.getFixtures().fixturesPath = 'spec/fixtures/';
-		loadFixtures('init.html');
-	});
+function callMyCallback(cb) {
+    cb();
+}
 
-	it("should p be appended to div.sandbox", function(){
-		spyOn($, "ajax");
-		bpf.callAjax();
-		$.ajax.calls.mostRecent().args[0].success('<p>teste</p>');
-		expect($('.sandbox').find('p').length).toEqual(1)
-	});
+describe("first test with jasmine-jquery", function() {
+    beforeEach(function() {
+        jasmine.getFixtures().fixturesPath = 'spec/fixtures/';
+        loadFixtures('init.html');
+    });
+
+    it("should p be appended to div.sandbox", function() {
+        spyOn($, "ajax");
+        bpf.callAjax();
+        $.ajax.calls.mostRecent().args[0].success('<p>teste</p>');
+        expect($('.sandbox').find('p').length).toEqual(1)
+    });
+
+    it("shold call my callback", function() {
+        var spyCB = jasmine.createSpy("mySpy");
+        callMyCallback(spyCB);
+        expect(spyCB).toHaveBeenCalled();
+    });
+
+    it("shold call my callback with sinon", function() {
+        var spyCB = sinon.spy();
+        callMyCallback(spyCB);
+        expect(spyCB.called).toBe(true);
+    });
+
 });
